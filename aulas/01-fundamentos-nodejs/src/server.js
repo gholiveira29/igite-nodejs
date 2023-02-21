01697981e3fbe9c415e65c808faf00f484b0f000
Guilherme Oliveira <g.h.p.o3615@gmail.com>
2023-02-21 01:44:36 -0300

    primeiras aulas

---
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,20 @@
+import http from 'node:http';
+const users = [];
+const server = http.createServer((req, res) => {
+    const { method, url } = req;
+    if (method === 'GET' && url === '/users') {
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users));
+    }
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            name: 'Guilerme',
+            email: 'teste@gmail.com',
+            id: 1
+        });
+        return res.writeHead(201).end();
+    }
+    return res.writeHead(404).end();
+});
+server.listen(3333);
