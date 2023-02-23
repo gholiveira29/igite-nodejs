099a947646f22816372bce06760ab62ab8cf5512
Guilherme Oliveira <g.h.p.o3615@gmail.com>
2023-02-23 17:49:14 -0300

    persistindo os dados do banco local

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,21 +1,15 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
-const users = [];
+import { routes } from './middlewares/routes.js';
+
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
     await json(req, res);
-
-    if (method === 'GET' && url === '/users') {
-        return res.end(JSON.stringify(users));
-    }
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body
-        users.push({
-            name,
-            email,
-            id: Math.random()
-        });
-        return res.writeHead(201).end();
+    const route = routes.find(route => {
+        return route.method === method && route.path === url;
+    });
+    if (route) {
+        return route.handler(req, res);
     }
     return res.writeHead(404).end();
 });
