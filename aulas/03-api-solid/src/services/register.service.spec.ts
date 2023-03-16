80012dfd1bef80408e384a49a37592de0d3cbeb5
Guilherme Oliveira <g.h.p.o3615@gmail.com>
2023-03-16 18:22:19 -0300

    criando teste unitario

---
+++ aulas/03-api-solid/src/services/register.service.spec.ts
@@ -0,0 +1,43 @@
+import { UserAlreadyExistsError } from './error/user-already-exists-error';
+import { compare } from 'bcryptjs';
+import { RegisterService } from './register.service';
+import { expect, describe, it } from 'vitest';
+import { InMemoryUsersRepository } from '@/repositories/inMemory/inMemoryUsersRepository';
+
+describe('Register Use Case', () => {
+    it('Should hash user password upon registration', async () => {
+        const inMemoryUsersRepository = new InMemoryUsersRepository();
+        const registerService = new RegisterService(inMemoryUsersRepository);
+
+        const { user } = await registerService.execute({
+            name: 'Henrique',
+            email: 'henriqueTeste@gmail.com',
+            password: '12345678910'
+        });
+
+        const isPasswordCorrectlyHash = await compare('12345678910', user.password_hash);
+        expect(isPasswordCorrectlyHash).toBe(true);
+    });
+});
+
+it('should not be able to register with same email twice', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerService = new RegisterService(inMemoryUsersRepository);
+
+    const email = 'henriqueTeste@gmail.com';
+
+    await registerService.execute({
+        name: 'Henrique',
+        email,
+        password: '12345678910'
+    });
+
+    expect(() =>
+        registerService.execute({
+            name: 'Henrique',
+            email,
+            password: '12345678910'
+        })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
+
+});
--- aulas/03-api-solid/src/services/register.service.ts
+++ aulas/03-api-solid/src/services/register.service.ts
@@ -1,6 +1,7 @@
 import { UserAlreadyExistsError } from './error/user-already-exists-error';
 import { UsersRepository } from './../repositories/users-repository';
 import { hash } from "bcryptjs";
+import { User } from '@prisma/client';
 
 interface RegisterServiceRequest {
     password: string;
@@ -8,11 +9,15 @@ interface RegisterServiceRequest {
     name: string;
 }
 
+interface RegisterServiceResponse {
+    user: User;
+}
+
 export class RegisterService {
     constructor(private usersRepository: UsersRepository) {
 
     }
-    async execute({ name, email, password }: RegisterServiceRequest) {
+    async execute({ name, email, password }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
         const password_hash = await hash(password, 6);
         const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
@@ -20,11 +25,13 @@ export class RegisterService {
             throw new UserAlreadyExistsError();
         }
 
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password_hash
         });
+
+        return { user };
     }
 }
 
